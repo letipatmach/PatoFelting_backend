289c39c2fd2b96d39d3ed0a12c22997fd117ec71
Leticia <letipatmach@gmail.com>
2023-11-01 22:09:20 -0300

    add p.json,modeloarticulo y getListarticulo

--- database/conexion.js
+++ database/conexion.js
@@ -1,2 +1,15 @@
-//aca nos vamos a conectar//
-//estamos bien???//
+import mongoose from "mongoose";
+
+const urlDatabase =process.env.DB_LINK;
+  
+
+export const conectarDB = () => {
+  return mongoose
+    .connect(urlDatabase)
+    .then(() => {
+      console.log("Conectado a la DB Vivero!");
+    })
+    .catch((error) => {
+      console.log("Error conectando a la DB: ", error);
+    });
+};
